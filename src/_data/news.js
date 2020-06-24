0bbf7c7c5e0997ca3cf4907ed33efafdf03d82c9
Consuelo Sanna <consu.sanna@gmail.com>
2020-06-24 16:09:09 +0200

    automatic generate redirect page

--- src/_data/news.js
+++ src/_data/news.js
@@ -2,14 +2,26 @@ const { default: Axios } = require("axios");
 
 const axios = require("axios");
 require("dotenv").config();
+const countries = require("./countries.json");
 
-module.exports = async function () {
+async function getNews(country) {
   try {
     const response = await axios.get(
-      `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}&pageSize=5`
+      `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.API_KEY}&pageSize=5`
     );
-    return response.data;
+    return {
+      country: country,
+      articles: response.data.articles,
+    };
   } catch (error) {
     console.log(error);
   }
+}
+
+module.exports = async function () {
+  let newPromises = countries.map(getNews);
+  return Promise.all(newPromises).then((newsObjects) => {
+    console.log("newsObje ", newsObjects);
+    return [].concat.apply([], newsObjects);
+  });
 };
