805c098d22c427c0eb4c733ad46579fc798fe1cb
Consuelo Sanna <consu.sanna@gmail.com>
2020-06-24 15:05:25 +0200

    adding dynamic data

---
+++ src/_data/news.js
@@ -0,0 +1,15 @@
+const { default: Axios } = require("axios");
+
+const axios = require("axios");
+require("dotenv").config();
+
+module.exports = async function () {
+  try {
+    const response = await axios.get(
+      `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}&pageSize=5`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
